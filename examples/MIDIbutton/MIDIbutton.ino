199983e0d9e57f9acb06e7a50b577b398dd3a8d6
Tom Moxon <tom@moxon.com>
2019-01-14 13:55:23 -0800

    Add Serial MIDI Interfaces

--- examples/MIDIbutton/MIDIbutton.ino
+++ examples/MIDIbutton/MIDIbutton.ino
@@ -1,17 +1,30 @@
 #include "MIDIcontroller.h"
-byte MIDIchannel = 10;
-const int latchPin = 5; //any digital pin
-const int triggerPin = 3; //any Capacitive Touch capable pin
-const int ledPin = 13;   //Set an LED to show the state of a latch button.
+/* MIDIButton example :
+   Defaults :
+   Message      : Control Change
+   MIDI Output  : USB, Cable 0
+   MIDI Channel : 10 (Drum)
+
+*/
+const int leftPin = 4;        // any digital pin
+const int rightPin = 5;       // any digital pin
+const int ledPin = 13;        // Set an LED to show the state of a latch button.
 
 // MOMENTARY buttons are the default. LATCH or TRIGGER may also be set
-MIDIbutton latchButton(latchPin, 21, LATCH);       // Control Change #21
-MIDIbutton triggerButton(triggerPin, 15, TRIGGER); // Control Change #15
+MIDIbutton leftButton(leftPin,   21, LATCH);     // default to Control Change #21, USB, Cable 0, Channel 10
+MIDIbutton rightButton(rightPin, 15, MOMENTARY); // default to Control Change #15, USB, Cable 0, Channel 10
 
 void setup(){
   pinMode(ledPin, OUTPUT);
+  MIDI_setup();
+  rightButton.setChannel(1,0,0);           // change to channel 1
+  leftButton.setChannel(1,0,0);            // change to channel 1
+  leftButton.setOnMessage(0x90, 60, 127);  // change message to NoteOn, Key = 60, Velocity = fortissimo
+  leftButton.setOffMessage(0x80, 60, 0);   // change message to NoteOff, Key = 60, Velocity = none
 }
 void loop(){
-  latchButton.send();
-  digitalWrite(ledPin, latchButton.state);
+  MIDI_loop();
+  digitalWrite(ledPin, leftButton.state);
+  leftButton.send();
+  rightButton.send();
 }
